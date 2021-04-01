c2d92c050c79ba92ec5b47cd7379ed5167fbaa18
Parham Shagerdmootab <psm1379@gmail.com>
2021-04-01 14:46:47 -0700

    Added the tabbed activity

---
+++ project/app/src/main/java/com/example/project/view/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.project.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
